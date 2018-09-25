64653c784110037a2673555bda12c3d2edf58572
nicoleganung <ncg1246@rit.edu>
2018-09-25 15:41:45 -0400

    got the pop and push of general numbers

--- MaxRectanglePerimeter.java
+++ MaxRectanglePerimeter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  * Created by brendanmutton & nicoleganung on 9/23/18.
  */
@@ -10,100 +11,136 @@ public class MaxRectanglePerimeter {
         int numInputs = scan.nextInt();
 
         Node[] vertex_array = new Node[numInputs];
-        String [] shortStrArr = new String[2];
-        String [] strArr = new String[numInputs * 2];
+        String[] shortStrArr = new String[2];
+        String[] strArr = new String[numInputs * 2];
 
         scan.nextLine();
 
-        for(int i = 0; i < numInputs; i++)
-        {
+        for (int i = 0; i < numInputs; i++) {
             shortStrArr = scan.nextLine().split(" ");
             vertex_array[i] = new Node(Long.parseLong(shortStrArr[0]), Long.parseLong(shortStrArr[1]));
         }
 
-
         Stack stack = new Stack();
+        stack.push(vertex_array[1].getX(), vertex_array[1].getY());
 
+        for (i = 2; i < vertex_array.length(); i++) {
 
-        
-        // for retrieving x value, n/2
-        // for retrieving y value, n/2-1
+            if (vertex_array[i].getY() > vertex_array[i - 1].getY() && vertex_array[i].getY() != stack.peek().getY())
+                vertex_array[i].push(vertex_array[i].getX(), vertex_array[i].getY());
+            else if (vertex_array[i].getY() < stack.peek().getY()) {
+
+                while (stack.peek().getY() < vertex_array[i].getY()) {
+                    calcPerimeter();
+                    stack.pop();
+                }
+            }
+        }
     }
 
     /*
      */
-    public long calcPerimeter(long x_1, long x_2, long height){
-        return ((x_2 - x_1)*2) + (height*2);
+    public long calcPerimeter(long x_1, long x_2, long height) {
+        return ((x_2 - x_1) * 2) + (height * 2);
     }
 }
 
-class Node{
+/*
+ */
+class Node {
     public long x;
     public long y;
     public Node next;
 
-
-    public Node(long x, long y)
-    {
+    /*
+     */
+    public Node(long x, long y) {
         this.x = x;
         this.y = y;
     }
 
-    public void viewNode()
-    {
-        System.out.println("("+this.x + ", " + this.y+")");
+    /*
+     */
+    public long getX() {
+        return x;
+    }
+
+    /*
+     */
+    public long getY() {
+        return y;
+    }
+
+    /*
+     */
+    public void viewNode() {
+        System.out.println("(" + this.x + ", " + this.y + ")");
         System.out.println("");
     }
 }
 
-class LinkedList{
+/*
+ */
+class LinkedList {
+
     public Node first = null;
 
-    public void insert(long x, long y)
-    {
+    /*
+     */
+    public void insert(long x, long y) {
         Node n = new Node(x, y);
         n.next = first;
         first = n;
     }
 
-    public Node delete_head()
-    {
+    /*
+     */
+    public Node delete_head() {
         Node temp = first;
-
         first = first.next;
-
         return temp;
     }
 
-    public void printList()
-    {
+    /*
+     */
+    public void printList() {
         Node n = first;
-        while(n != null)
-        {
+
+        while (n != null) {
             n.viewNode();
             n = n.next;
         }
     }
 
-    public boolean isEmpty()
-    {
+    /*
+     */
+    public boolean isEmpty() {
         return first == null;
     }
 }
 
-class Stack{
+/*
+ */
+class Stack {
+
     LinkedList link = new LinkedList();
 
-    public void pop(){
+    /*
+     */
+    public void pop() {
         link.delete_head();
     }
-    public void push(long x, long y){
-        link.insert(x, y);
 
+    /*
+     */
+    public void push(long x, long y) {
+        link.insert(x, y);
     }
-    public Node peek()
-    {
-        return link.delete_head();
+
+    /*
+     */
+    public Node peek() {
+        return link.first;
     }
 }
 
