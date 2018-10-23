2b303d059ee641e564f42072e1ea1382df70a36e
nicole.ganung@outlook.com <iisixstIISIXST97>
2018-10-23 16:13:06 -0400

    input copied from previous HW

---
+++ HW4/LongestConvexSubseq.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/*
+ * Created by Nicole Ganung and Brendan Mutton
+ */
+public class LongestConvexSubseq{
+
+    public static long inputArr[];
+
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+        int numInputs = sc.nextInt();
+        sc.nextLine();
+
+        String[] inputs;
+        inputs = sc.nextLine().split(" ");
+        inputArr = new long[numInputs];
+
+        for (int i = 0; i < inputs.length; i++)
+            inputArr[i] = Long.parseLong(inputs[i]);
+
+        longestConvexSubseq(inputArr);
+    }
+
+    public void longestConvexSubseq(long[] inputArr){
+        return;
+    }
+
+}
