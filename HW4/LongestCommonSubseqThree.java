c27f7d2a5f33b31dbe4c6e7870fb688d20fbde7d
nicoleganung <ncg1246@rit.edu>
2018-10-30 22:40:17 -0400

    working for 2D array

--- HW4/LongestCommonSubseqThree.java
+++ HW4/LongestCommonSubseqThree.java
@@ -38,6 +38,24 @@ public class LongestCommonSubseqThree{
     }
 
     public static void longestCommonSubseq(long[] a, long[] b, long[] c){
+        // TODO: Make into 3D solution array
+        int[][] sol = new int[a.length][b.length];
+
+        for(int j=1; j<a.length; j++){
+            for(int k=1; k<b.length; k++) {
+                if((sol[j-1][k] > sol[j][k-1]) || sol[j-1][k] == sol[j][k-1]){
+                    sol[j][k] = sol[j-1][k];
+                } else if(sol[j-1][k] < sol[j][k-1]) {
+                    sol[j][k] = sol[j][k-1];
+                }
+                if(a[j] == b[k]){
+                    sol[j][k] = (sol[j-1][k-1]) + 1;
+                }
+                System.out.print(sol[j][k]);
+            }
+            System.out.println("");
+        }
+        System.out.println((sol[a.length-1][b.length-1])-1);
         return;
     }
 
