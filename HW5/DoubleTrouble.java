27bbf22368b2260f33b5d57d31978bba84ae14aa
nicole.ganung@outlook.com <iisixstIISIXST97>
2018-11-17 21:42:07 -0500

    created vertex and graph

---
+++ HW5/DoubleTrouble.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/*
+ * Created by Brendan Mutton & Nicole Ganung
+ */
+
+public class DoubleTree{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int rows = scan.nextInt();
+        int cols = scan.nextInt();
+
+        String rowsArr[] = new rowsArr[];
+
+        for(int i = 0; i<rows; i++){
+            rowsArr[i] = scan.nextLine();
+        }
+
+        spanTree(rowsArr);
+    }
+
+    public
+}
--- HW5/Prerequisites.java
+++ HW5/Prerequisites.java
@@ -3,19 +3,68 @@ import java.util.Scanner;
  * Created by Brendan Mutton & Nicole Ganung
  */
 
-public class Prerequisites{
+class Prerequisites{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        for(int i = 0; i<n; i++){
-            scan.nextLine();
+        Vertex[] courses = new Vertex[n];
+
+        for(int i = 1; i<n; i++){
+            Vertex v = new Vertex(i);
+            courses[i] = v;
         }
 
-        prereqs();
+        Graph g = new Graph(courses);
     }
+}
 
-    public static void prereqs(){
+class Sort{
+    public Sort(){
+
+    }
+    public int dfs(Graph g){
+        return 0;
+    }
+    public void sort(){
 
     }
 }
+
+class Graph{
+    public Vertex courses[];
+
+    public Graph(Vertex[] courses){
+        this.courses = courses;
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0; i < courses.length; i++){
+            String pre = scan.nextLine();
+            String[] preReqs = pre.split(" ");
+            courses[i].addEdges(preReqs);
+        }
+    }
+}
+
+class Edge{
+    public int course,prereq;
+
+    public Edge(int course, int prereq){
+        this.course = course;
+        this.prereq = prereq;
+    }
+}
+
+class Vertex{
+    public int courseNumber;
+    public Edge[] edges;
+    public int counter = 0;
+
+    public Vertex(int courseNumber){
+        this.courseNumber = courseNumber;
+    }
+
+    public void addEdges(String[] arr){
+        //add edges
+    }
+}
+
