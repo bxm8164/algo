448c5cfbf4a713394e2ecf4d0b272b0eb7a54d46
nicole.ganung@outlook.com <iisixstIISIXST97>
2018-12-10 22:59:56 -0500

    spacing

--- HW6/NetworkConnect.java
+++ HW6/NetworkConnect.java
@@ -30,13 +30,10 @@ public class NetworkConnect{
             int u = Integer.parseInt(b[0]), v = Integer.parseInt(b[1]), w = Integer.parseInt(b[2]);
             graph[u][v] = w;
         }
-
         edmondsKarp(graph, s, t);
-
     }
 
-    public static boolean bfs(int rGraph[][], int s, int t, int parent[])
-    {
+    public static boolean bfs(int rGraph[][], int s, int t, int parent[]) {
         boolean visited[] = new boolean[numComputers];
         for(int i=0; i<numComputers; ++i)
             visited[i]=false;
@@ -46,27 +43,21 @@ public class NetworkConnect{
         visited[s] = true;
         parent[s]=-1;
 
-        while (!queue.isEmpty())
-        {
+        while (!queue.isEmpty()) {
             int u = queue.dequeue();
 
-            for (int v=0; v<numComputers; v++)
-            {
-                if (!visited[v] && rGraph[u][v] > 0)
-                {
+            for (int v=0; v<numComputers; v++) {
+                if (!visited[v] && rGraph[u][v] > 0) {
                     queue.enqueue(v);
                     parent[v] = u;
                     visited[v] = true;
-
                 }
             }
         }
-
         return (visited[t]);
     }
 
-    public static void edmondsKarp(int graph[][], int s, int t)
-    {
+    public static void edmondsKarp(int graph[][], int s, int t) {
         int u, v;
 
         int rGraph[][] = new int[numComputers][numConnections];
@@ -77,51 +68,40 @@ public class NetworkConnect{
 
         int parent[] = new int[numComputers];
 
-        while (bfs(rGraph, s, t, parent))
-        {
+        while (bfs(rGraph, s, t, parent)) {
             int path_flow = Integer.MAX_VALUE;
-            for (v=t; v!=s; v=parent[v])
-            {
+            for (v=t; v!=s; v=parent[v]) {
                 u = parent[v];
                 path_flow = Math.min(path_flow, rGraph[u][v]);
             }
-            for (v=t; v != s; v=parent[v])
-            {
+            for (v=t; v != s; v=parent[v]) {
                 u = parent[v];
                 rGraph[u][v] -= path_flow;
                 rGraph[v][u] += path_flow;
             }
-
         }
 
         bfs(rGraph, s, t, parent);
-
         sort(parent);
 
-        for(int i = 1; i < parent.length; i++)
-        {
-            if(rGraph[source][i] == 0 && i != source)
-            {
+        for(int i = 1; i < parent.length; i++) {
+            if(rGraph[source][i] == 0 && i != source) {
                 System.out.println(source +" "+ i);
                 System.exit(0);
             }
         }
         System.out.println("NO");
-
     }
-    public static void sort(int arr[])
-    {
+
+    public static void sort(int arr[]) {
         array = arr;
         len = arr.length;
         temp = new int[len];
         mergeSort(0, len-1);
-
     }
 
-    private static void mergeSort(int lower, int higher)
-    {
-        if(lower < higher)
-        {
+    private static void mergeSort(int lower, int higher) {
+        if(lower < higher) {
             int mid = lower + (higher - lower) / 2;
             mergeSort(lower, mid);
             mergeSort(mid + 1, higher);
@@ -129,41 +109,33 @@ public class NetworkConnect{
         }
     }
 
-    public static void merge(int lower, int mid, int higher)
-    {
-        for(int i = lower; i <= higher; i++)
-        {
+    public static void merge(int lower, int mid, int higher) {
+        for(int i = lower; i <= higher; i++) {
             temp[i] = array[i];
         }
         int i = lower;
         int j = mid + 1;
         int k = lower;
-        while(i <= mid && j <= higher)
-        {
-            if(temp[i] <= temp[j])
-            {
+        while(i <= mid && j <= higher) {
+            if(temp[i] <= temp[j]) {
                 array[k] = temp[i];
                 i++;
             }
-            else
-            {
+            else {
                 array[k] = temp[j];
                 j++;
             }
             k++;
         }
-        while(i <= mid)
-        {
+        while(i <= mid) {
             array[k] = temp[i];
             k++;
             i++;
         }
     }
-
 }
 
 class Queue<T> {
-
 
     private class Node{
         T item;
@@ -173,12 +145,10 @@ class Queue<T> {
             item = t;
             next = null;
         }
-
         public String toString()  {
             return  item.toString();
         }
     }
-
 
     private Node first, last;
 
@@ -186,7 +156,6 @@ class Queue<T> {
         first = null;
         last = null;
     }
-
 
     public boolean isEmpty(){
         return first == null;
@@ -213,22 +182,4 @@ class Queue<T> {
         first = first.next;
         return t;
     }
-/*
-    public String toString(){
-        if (first == null)
-            return null;
-
-        if (first == last)
-            return first.item.toString();
-
-        StringBuffer sb = new StringBuffer();
-        Node cur = first;
-        while(cur != last){
-            sb.append(cur.item.toString()+" ");
-            cur = cur.next;
-        }
-        sb.append(last.item.toString());
-        return sb.toString();
-    }
-*/
 }
